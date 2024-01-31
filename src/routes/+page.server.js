c3f00b51754f3b71d10f9ef3ab9695e0c36d143a
Daniel Boadzie <boadziedaniel43@gmail.com>
2024-01-31 11:50:15 +0000

    Add page server file to contain business logic
    - Add action to get form data
    - Add a utility function to use the text input to generate a response
    - Add a load function that share the result from the input through the function to the frontend

---
+++ src/routes/+page.server.js
@@ -0,0 +1,35 @@
+import { COHERE_API_KEY } from '$env/static/private';
+
+import { CohereClient } from 'cohere-ai';
+
+export const load = async ({ locals }) => {
+	const keys = await getKeys(locals.text);
+	// const results = keys;
+	// console.log(keys);
+	return {
+		results: keys
+	};
+};
+
+async function getKeys(text) {
+	const cohere = new CohereClient({
+		token: COHERE_API_KEY
+	});
+
+	const results = await cohere.generate({
+		prompt: `Extract relevant keywords from this job description ${text}.Keywords must be relevant to the job in question. Return only the list with comma separation. Dont add any other thing. `
+	});
+	return results.generations[0].text;
+}
+
+export const actions = {
+	default: async ({ request, locals }) => {
+		const formData = await request.formData();
+		const text = formData.get('text');
+
+		locals.text = text;
+		return {
+			success: true
+		};
+	}
+};
