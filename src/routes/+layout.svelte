e66bf7c2b7cd8832998ab1ee5690f5c9179d2f1c
Daniel Boadzie <boadziedaniel43@gmail.com>
2024-01-31 10:23:10 +0000

    Start Keystar App
    - Add UnoCSS
    - Add Yesvelte for styling
    - Add Navbar to project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import tabler from 'yesvelte/css/tabler.min.css?url';
+	import 'uno.css';
+	import { Navbar, NavbarItem } from 'yesvelte';
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href={tabler} />
+</svelte:head>
+<Navbar theme="dark">
+	<NavbarItem icon="home" title="Home" />
+	<NavbarItem icon="star" title="About" />
+</Navbar>
+<main>
+	<slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { El } from 'yesvelte';
+</script>
+
+<section class="container mx-auto px-4 py-4">
+	<El tag="h1" class="text-slate-500">Keystar ⭐</El>
+</section>
