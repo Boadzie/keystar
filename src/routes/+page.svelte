bfc51c8e71ee633bb7b56c94126a1d054fb3275c
Daniel Boadzie <boadziedaniel43@gmail.com>
2024-01-31 12:38:42 +0000

    Add application description and usage instructions and change badge type

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { CardBody, El, Card, FormTextarea, Button, Badge } from 'yesvelte';
+	import { CardBody, El, Card, Textarea, Button, Badge } from 'yesvelte';
 
 	export let data;
 	const { results } = data;
@@ -8,31 +8,38 @@
 
 <section class="container mx-auto lg:px-24 py-4">
 	<El tag="h1" class="text-slate-500">Keystar ⭐</El>
+	<El tag="p" class="text-lg"
+		>Transform your job description into a personalized resume (CV) with tailored keywords for an
+		unmatched application experience! <br /> Enter the job details below to receive relevant keywords
+		for your CV.
+	</El>
 	<div class="grid grid-cols-2 gap-4 place-content-center">
 		<Card>
 			<CardBody>
-				<form method="POST">
-					<FormTextarea
-						class=""
-						label="text"
+				<form method="POST" class="">
+					<Textarea
+						class="min-h-64 mb-4"
+						rows="10"
 						name="text"
-						required
 						placeholder="Enter your text here..."
-					/>
+					></Textarea>
+
 					<Button type="submit">Submit</Button>
 				</form>
 			</CardBody>
 		</Card>
-		<Card>
+		<Card class="">
 			<CardBody>
-				<div class="grid grid-cols-4 gap-2 m-4">
-					{#each keys as key}
-						<div>
-							<Badge ghost color="dark" class="text-start text-wrap">
-								{key}
-							</Badge>
-						</div>
-					{/each}
+				<div class="flex flex-wrap">
+					{#if keys}
+						{#each keys as key}
+							<div>
+								<Badge ghost color="dark" class="text-start text-md font-bold m-2">
+									{key || ''}
+								</Badge>
+							</div>
+						{/each}
+					{/if}
 				</div>
 			</CardBody>
 		</Card>
