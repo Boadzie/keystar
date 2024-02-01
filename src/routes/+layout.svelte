e317d60ef63ac21ecba76ff4879fde646c96febc
Daniel Boadzie <boadziedaniel43@gmail.com>
2024-02-01 20:51:18 +0000

    Add href attributes to navbar items

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,8 +8,8 @@
 	<link rel="stylesheet" href={tabler} />
 </svelte:head>
 <Navbar class="font-bold">
-	<NavbarItem class="hover:text-blue-500" icon="home" title="Home" />
-	<NavbarItem class="hover:text-blue-500" icon="phone" title="About" />
+	<NavbarItem class="hover:text-blue-500" href="/" icon="home" title="Home" />
+	<NavbarItem class="hover:text-blue-500" href="/" icon="phone" title="About" />
 </Navbar>
 <main>
 	<slot />
